47d9224e23482105d99feb67a54fc943d72f0752
Mwawaka <kedskeds90@gmail.com>
2022-05-26 19:50:59 +0300

    Commit

--- market/models.py
+++ market/models.py
@@ -1,17 +1,16 @@
-from sqlalchemy import ForeignKey
 from market import db
 
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    username = db.Column(db.String(Length=30), nullable=False, unique=True)
-    email_address = db.Column(db.String(Length=50),
+    username = db.Column(db.String(length=30), nullable=False, unique=True)
+    email_address = db.Column(db.String(length=50),
                               nullable=False, unique=True)
-    password_hash = db.Column(db.String(), nullable=False)
+    password_hash = db.Column(db.String(length=60), nullable=False)
     budget = db.Column(db.Integer(), nullable=False, default=1000)
-    items = db.Column('Item',backref='owned_user',lazy=True)
+    items = db.relationship('Item',backref='owned_user',lazy=True)
 
-    def __repr__(self) -> str:
+    def __repr__(self):
         return f'User {self.username}'
 
 
@@ -21,7 +20,7 @@ class Item(db.Model):
     price = db.Column(db.Integer(), nullable=False)
     barcode = db.Column(db.String(length=12), nullable=False, unique=True)
     description = db.Column(db.String(length=1024), nullable=False, unique=True)
-    owner=db.Column(db.Integer(),db<ForeignKey('user.id'))
-    
-    def __repr__(self) -> str:
-        return f'User {self.name}'
+    owner=db.Column(db.Integer(),db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f'Item {self.name}'
