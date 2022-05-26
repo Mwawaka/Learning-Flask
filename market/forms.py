903ecab4c5f67d02ae10ef7d8fe9b9739767fd5a
Mwawaka <kedskeds90@gmail.com>
2022-05-26 20:53:38 +0300

    Commit

--- market/forms.py
+++ market/forms.py
@@ -1,10 +1,14 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField,EmailField,PasswordField
+from wtforms import StringField, SubmitField, EmailField, PasswordField
+from wtforms.validators import Length, Email, DataRequired, EqualTo
 
 
 class RegisterForm(FlaskForm):
-    username=StringField(label='Username:')
-    email_address=EmailField(label='Email Address:')
-    password1=PasswordField(label='Password:')
-    password2=PasswordField(label='Confirm Password:')
-    submit=SubmitField(label='Create Account')
+    username = StringField(label='Username:', validators=[
+                           Length(min=2, max=30), DataRequired()])
+    email_address = EmailField(label='Email Address:', validators=[
+                               Email(), DataRequired()])
+    password1 = PasswordField(label='Password:',validators=[Length(min=6), DataRequired()])
+    password2 = PasswordField(
+        label='Confirm Password:', validators=[EqualTo(password1)])
+    submit = SubmitField(label='Create Account')
