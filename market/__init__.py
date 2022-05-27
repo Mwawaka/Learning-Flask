816ebb881b583923295ae01ece2fe2c0271a60a3
Mwawaka <kedskeds90@gmail.com>
2022-05-27 09:18:39 +0300

    Commit

--- market/__init__.py
+++ market/__init__.py
@@ -1,12 +1,11 @@
-from market import routes
-import bcrypt
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
-
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '44de154f207a4c739f20ec82d91c8474d6c139402e5cb11473920dae31e57d0d'
 bcrypt = Bcrypt(app)
+
+from market import routes
