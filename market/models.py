dd4c52cce8d69148e771a745bbe638fcc922d02b
Mwawaka <kedskeds90@gmail.com>
2022-05-29 19:08:13 +0300

    Commit

--- market/models.py
+++ market/models.py
@@ -1,9 +1,12 @@
 from market import db
 from market import bcrypt
+from market import login_manager
+from flask_login import UserMixin
 
-
-@login_manager
-class User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+class User(db.Model,UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
     email_address = db.Column(db.String(length=50),
