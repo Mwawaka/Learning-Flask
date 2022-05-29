eeb7d821ca58630bd38279ee7dbb99cc43f9aef8
Mwawaka <kedskeds90@gmail.com>
2022-05-29 19:00:40 +0300

    Commit

--- market/__init__.py
+++ market/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 app.config['SECRET_KEY'] = '44de154f207a4c739f20ec82d91c8474d6c139402e5cb11473920dae31e57d0d'
 bcrypt = Bcrypt(app)
+login_manager=LoginManager(app)
 
 from market import routes
--- market/models.py
+++ market/models.py
@@ -2,6 +2,7 @@ from market import db
 from market import bcrypt
 
 
+@login_manager
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     username = db.Column(db.String(length=30), nullable=False, unique=True)
